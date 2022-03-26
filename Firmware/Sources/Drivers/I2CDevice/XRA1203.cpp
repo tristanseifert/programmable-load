f1a35f6909ad21a84e603e807f06dbe168bf4fef
Tristan Seifert <tristan@tseifert.me>
2022-03-25 19:14:49 -0700

    I2C driver for XRA1203
    - Start work on a device driver for the XRA1203 IO expander, which is
      used on the front panel

---
+++ Firmware/Sources/Drivers/I2CDevice/XRA1203.cpp
@@ -0,0 +1,261 @@
+#include "XRA1203.h"
+
+#include "Drivers/I2CBus.h"
+#include "Log/Logger.h"
+#include "Rtos/Rtos.h"
+
+#include <etl/array.h>
+
+using namespace Drivers::I2CDevice;
+
+/**
+ * @brief Initialize the IO expander.
+ *
+ * All pins are configured according to the specified pin configuration map. These settings are
+ * fixed, for the most part, until the part is reset/reinitialized.
+ *
+ * @param bus I²C bus to which the expander is connected
+ * @param pins A list of 16 pin configuration entries, one for each IO pin
+ * @param address I²C address of the device
+ */
+XRA1203::XRA1203(Drivers::I2CBus *bus, etl::span<const PinConfig, kIoLines> pins,
+        const uint8_t address) : bus(bus), deviceAddress(address) {
+    int err;
+    uint16_t ocr{0}, pir{0}, gcr{0}, pur{0}, ier{0}, tscr{0}, reir{0}, feir{0}, ifr{0};
+
+    for(size_t i = 0; i < kIoLines; i++) {
+        const auto &cfg = pins[i];
+        const uint16_t bit = 1U << static_cast<uint16_t>(i);
+
+        // input specific
+        if(cfg.input) {
+            // enable as input
+            gcr |= bit;
+
+            // interrupts
+            if(cfg.irq) {
+                ier |= bit;
+                if(cfg.irqRising) {
+                    reir |= bit;
+                }
+                if(cfg.irqFalling) {
+                    feir |= bit;
+                }
+
+                // irq filtering
+                if(cfg.irqFilter) {
+                    ifr |= bit;
+                }
+            }
+            // pull ups
+            if(cfg.pullUp) {
+                pur |= bit;
+            }
+            // polarity inversion
+            if(cfg.invertInput) {
+                pir |= bit;
+            }
+        }
+        // output specific
+        else {
+            // enable as output
+            gcr &= ~bit;
+            // initial output state
+            if(cfg.initialOutput) {
+                ocr |= bit;
+            }
+            // tristate
+            if(cfg.tristated) {
+                tscr |= bit;
+            }
+        }
+    }
+
+    // save shadow registers
+    this->gpioConfig = gcr;
+    this->output = ocr;
+    this->tristate = tscr;
+
+    // then write the actual registers
+    err = this->writeRegister(Register::OCR1, ocr);
+    REQUIRE(!err, "%s: failed to write register %s (%d)", "XRA1203", "OCR", err);
+    err = this->writeRegister(Register::PIR1, pir);
+    REQUIRE(!err, "%s: failed to write register %s (%d)", "XRA1203", "PIR", err);
+    err = this->writeRegister(Register::PUR1, pur);
+    REQUIRE(!err, "%s: failed to write register %s (%d)", "XRA1203", "PUR", err);
+    err = this->writeRegister(Register::GCR1, gcr);
+    REQUIRE(!err, "%s: failed to write register %s (%d)", "XRA1203", "GCR", err);
+    err = this->writeRegister(Register::TSCR1, tscr);
+    REQUIRE(!err, "%s: failed to write register %s (%d)", "XRA1203", "TSCR", err);
+    err = this->writeRegister(Register::REIR1, reir);
+    REQUIRE(!err, "%s: failed to write register %s (%d)", "XRA1203", "REIR", err);
+    err = this->writeRegister(Register::FEIR1, feir);
+    REQUIRE(!err, "%s: failed to write register %s (%d)", "XRA1203", "FEIR", err);
+    err = this->writeRegister(Register::IFR1, ifr);
+    REQUIRE(!err, "%s: failed to write register %s (%d)", "XRA1203", "IFR", err);
+    err = this->writeRegister(Register::IER1, ier);
+    REQUIRE(!err, "%s: failed to write register %s (%d)", "XRA1203", "IER", err);
+}
+
+/**
+ * @brief Set the state of an output pin
+ *
+ * This internally updates the shadow register, then writes back the state for a bank of 8 pins at
+ * a time to avoid the overhead of an extra read transaction to the device.
+ *
+ * @param pin Pin number to set ([0,15])
+ * @param state New state of the pin; true = set
+ *
+ * @return 0 on success, or an error code
+ */
+int XRA1203::setOutput(const uint8_t pin, const bool state) {
+    if(pin >= kIoLines) {
+        return Errors::InvalidPin;
+    }
+
+    // update shadow register
+    const uint16_t bit = 1U << static_cast<uint16_t>(pin);
+    if(state) {
+        this->output |= bit;
+    } else {
+        this->output &= ~bit;
+    }
+
+    // set the appropriate half of the register
+    if(pin >= 8) {
+        return this->writeRegister(Register::OCR1, static_cast<uint8_t>(this->output >> 8));
+    } else {
+        return this->writeRegister(Register::OCR2, static_cast<uint8_t>(this->output & 0xFF));
+    }
+}
+
+/**
+ * @brief Set whether a pin is tristated
+ *
+ * @param pin Pin number to set ([0,15])
+ * @param isTristate Whether we tristate the pin
+ *
+ * @return 0 on success, or an error code
+ */
+int XRA1203::setOutputTristate(const uint8_t pin, const bool isTristate) {
+    if(pin >= kIoLines) {
+        return Errors::InvalidPin;
+    }
+
+    // update shadow register
+    const uint16_t bit = 1U << static_cast<uint16_t>(pin);
+    if(isTristate) {
+        this->tristate |= bit;
+    } else {
+        this->tristate &= ~bit;
+    }
+
+    // set the appropriate half of the register
+    if(pin >= 8) {
+        return this->writeRegister(Register::TSCR1, static_cast<uint8_t>(this->tristate >> 8));
+    } else {
+        return this->writeRegister(Register::TSCR2, static_cast<uint8_t>(this->tristate & 0xFF));
+    }
+}
+
+
+
+/**
+ * @brief Write the upper and lower part of a register
+ *
+ * Writes the 16-bit value to two consecutive registers. All registers are laid out with the
+ * register 1 having the high 8 bits, and register 2 the low 8 bits.
+ *
+ * @param reg Register to write
+ * @param value 16-bit value to write to register
+ *
+ * @return 0 on success, or an error code
+ */
+int XRA1203::writeRegister(const Register reg, const uint16_t value) {
+    etl::array<uint8_t, 3> request{
+        static_cast<uint8_t>(reg), static_cast<uint8_t>((value & 0xFF00) >> 8),
+        static_cast<uint8_t>(value & 0x00FF),
+    };
+
+    etl::array<I2CBus::Transaction, 1> txns{{
+        {
+            .address = this->deviceAddress,
+            .read = 0,
+            .length = 3,
+            .data = request
+        },
+    }};
+
+    return this->bus->perform(txns);
+}
+
+/**
+ * @brief Write a single register
+ *
+ * Write a byte to a single register
+ *
+ * @param reg Register to write
+ * @param value 8-bit value to write to register
+ *
+ * @return 0 on success, or an error code
+ */
+int XRA1203::writeRegister(const Register reg, const uint8_t value) {
+    etl::array<uint8_t, 2> request{
+        static_cast<uint8_t>(reg), value,
+    };
+
+    etl::array<I2CBus::Transaction, 1> txns{{
+        {
+            .address = this->deviceAddress,
+            .read = 0,
+            .length = 2,
+            .data = request
+        },
+    }};
+
+    return this->bus->perform(txns);
+}
+
+/**
+ * @brief Read the upper and lower part of a register
+ *
+ * @param reg First (high) register to read
+ * @param outValue Variable to receive the resulting value
+ *
+ * @return 0 on success, or an error code
+ */
+int XRA1203::readRegister(const Register reg, uint16_t &outValue) {
+    int err;
+
+    etl::array<uint8_t, 1> request{
+        static_cast<uint8_t>(reg)
+    };
+    etl::array<uint8_t, 2> reply;
+
+    etl::array<I2CBus::Transaction, 2> txns{{
+        // write address
+        {
+            .address = this->deviceAddress,
+            .read = 0,
+            .length = 1,
+            .data = request
+        },
+        // read two bytes of register data
+        {
+            .address = this->deviceAddress,
+            .read = 1,
+            .continuation = 1,
+            .length = 2,
+            .data = reply
+        },
+    }};
+
+    err = this->bus->perform(txns);
+
+    if(!err) {
+        outValue = (static_cast<uint16_t>(reply[0]) << 8) | reply[1];
+    }
+
+    return err;
+}
+
---
+++ Firmware/Sources/Drivers/I2CDevice/XRA1203.h
@@ -0,0 +1,207 @@
+#ifndef DRIVERS_I2CDEVICE_XRA1203_H
+#define DRIVERS_I2CDEVICE_XRA1203_H
+
+#include <stdint.h>
+
+#include <etl/span.h>
+
+namespace Drivers {
+class I2CBus;
+}
+
+namespace Drivers::I2CDevice {
+/**
+ * @brief XRA1203 – 16-bit IO expander with I²C interface and interrupts
+ *
+ * It supports 16 external IO lines, divided into two ports of 8 lines each. Each line can be
+ * individually configured as an input (with optional, weak pull-up) or an output (which may be
+ * tristated.)
+ *
+ * Input lines may each be configured to generate an interrupt on rising and/or falling edges of
+ * the input signals; and additionally, each pin can individually have an input glitch filter
+ * enabled or disabled.
+ * */
+class XRA1203 {
+    public:
+        /// Number of total IO lines
+        constexpr static const size_t kIoLines{16};
+
+        /**
+         * @brief Error codes emitted by the device
+         */
+        enum Errors: int {
+            /**
+             * @brief Invalid pin number
+             *
+             * Specified pin number is invalid
+             */
+            InvalidPin                  = -5300,
+        };
+
+
+        /**
+         * @brief Configuration for a single IO expander pin
+         */
+        struct PinConfig {
+            /**
+             * @brief Input mode
+             *
+             * Whether the pin is operating as an input (1) or output (0)
+             */
+            uint8_t input:1{1};
+            /**
+             * @brief Internal pullup resistor enable
+             *
+             * If the pin is configured as an input, the pin will have a weak (~100 kΩ) internal
+             * resistor to VCC enabled.
+             */
+            uint8_t pullUp:1{0};
+            /**
+             * @brief Invert input polarity
+             *
+             * Invert the value read from the input state register for this pin.
+             */
+            uint8_t invertInput:1{0};
+            /**
+             * @brief Generate pin change interrupts
+             *
+             * Set to enable interrupts when the pin changes. It should be set in combination with
+             * one of the rising or falling edge modes, or behavior is indeterminate and may
+             * trigger on either edge.
+             *
+             * @remark Interrupts can only be generated for inputs.
+             */
+            uint8_t irq:1{0};
+            /**
+             * @brief Generate interrupt on rising edge
+             *
+             * Generate an interrupt when a rising edge is detected on the input.
+             */
+            uint8_t irqRising:1{0};
+            /**
+             * @brief Generate interrupt on falling edge
+             *
+             * Generate an interrupt when a falling edge is detected on the input.
+             */
+            uint8_t irqFalling:1{0};
+            /**
+             * @brief Interrupt filter enable
+             *
+             * When enabled, pulses on input lines must be greater than ~1µs to generate an
+             * interrupt.
+             */
+            uint8_t irqFilter:1{1};
+            /**
+             * @brief Initial output state
+             *
+             * If the pin is configured as an output, the initial state of the pin.
+             */
+            uint8_t initialOutput:1{0};
+            /**
+             * @brief Tristate output pin
+             *
+             * If configured as an output, the pin can be tristated.
+             */
+            uint8_t tristated:1{0};
+        };
+
+        /**
+         * @brief Pin configuration for an unused input
+         *
+         * Configures the pin as an input, with no interrupts or pull resistors.
+         */
+        constexpr static const PinConfig kPinConfigUnused{
+            .input = true,
+            .pullUp = false,
+            .invertInput = false,
+            .irq = false,
+            .irqRising = false,
+            .irqFalling = false,
+            .irqFilter = false,
+            .initialOutput = false,
+            .tristated = true,
+        };
+
+    public:
+        XRA1203(Drivers::I2CBus *bus, etl::span<const PinConfig, kIoLines> pins,
+                const uint8_t address);
+
+        int setOutput(const uint8_t pin, const bool state);
+        int setOutputTristate(const uint8_t pin, const bool isTristate);
+
+        /**
+         * @brief Read the state of all pins
+         *
+         * Get the state of all pins, including the currently driven state of outputs, and that of
+         * inputs (after applying the inversion, if enabled)
+         *
+         * @param outStates Variable to receive the pin state, where 1 = asserted
+         *
+         * @return 0 on success, or an error code
+         */
+        int readAllInputs(uint16_t &outStates) {
+            return this->readRegister(Register::GSR1, outStates);
+        }
+
+    private:
+        enum class Register: uint8_t {
+            GSR1                        = 0x00,
+            GSR2                        = 0x01,
+            OCR1                        = 0x02,
+            OCR2                        = 0x03,
+            PIR1                        = 0x04,
+            PIR2                        = 0x05,
+            GCR1                        = 0x06,
+            GCR2                        = 0x07,
+            PUR1                        = 0x08,
+            PUR2                        = 0x09,
+            IER1                        = 0x0A,
+            IER2                        = 0x0B,
+            TSCR1                       = 0x0C,
+            TSCR2                       = 0x0D,
+            ISR1                        = 0x0E,
+            ISR2                        = 0x0F,
+            REIR1                       = 0x10,
+            REIR2                       = 0x11,
+            FEIR1                       = 0x12,
+            FEIR2                       = 0x13,
+            IFR1                        = 0x14,
+            IFR2                        = 0x15,
+        };
+
+        int writeRegister(const Register reg, const uint8_t value);
+        int writeRegister(const Register reg, const uint16_t value);
+
+        int readRegister(const Register reg, uint8_t &outValue);
+        int readRegister(const Register reg, uint16_t &outValue);
+
+    private:
+        /// Parent bus
+        Drivers::I2CBus *bus;
+        /// Device address
+        uint8_t deviceAddress;
+
+        /**
+         * @brief GPIO configuration
+         *
+         * Shadow of registers GCR1 and GCR2, for configuring the mode of an IO pin. A set bit
+         * indicates the pin is configured as an input.
+         */
+        uint16_t gpioConfig{0xffff};
+        /**
+         * @brief Output value
+         *
+         * Shadow of registers OCR1 and OCR2, which hold the values written to output pins.
+         */
+        uint16_t output{0x0000};
+        /**
+         * @brief Output tristate
+         *
+         * Shadow of registers TSCR1 and TSCR2, which can set an output pin to be tristated if they
+         * are set. They do not apply for inputs.
+         */
+        uint16_t tristate{0x0000};
+};
+}
+
+#endif
