10c7e16afcd18c833137d1ca46abf7351c4dc882
Tristan Seifert <tristan@tseifert.me>
2022-08-24 09:27:26 -0700

    Refactor to use embedded-fw base
    - This shifts a lot of the common code (namely, libc and some other
      external stuff) out into a common base for all embedded firmware.
    
      Still to be moved is the ST HAL code; and some verification that
      floating point support (but sans double support) can work.

--- Firmware/External/libc/Sources/malloc/umm_malloc_cfgport.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIBC_UMM_MALLOC_CFGPORT_H
-#define LIBC_UMM_MALLOC_CFGPORT_H
-
-#include <stddef.h>
-
-#endif
--- Firmware/External/libc/Sources/stdlib/abort.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdlib.h"
-
-/**
- * Handles abnormal program conditions by breaking into the debugger.
- */
-void abort() {
-    asm volatile("bkpt 0xf0" ::: "memory");
-    while(1) {}
-}
